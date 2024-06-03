5ee794b0bd6a9948965a64fcd0b2f11a05081e2c
denverwilliams <denver@debian.nz>
2024-06-03 22:58:47 +0000

    init

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,10 +1,31 @@
-/** @type import('hardhat/config').HardhatUserConfig */
-module.exports = {
-  solidity: "0.8.20",
+// #<{(|* @type import('hardhat/config').HardhatUserConfig |)}>#
+// module.exports = {
+//   solidity: "0.8.20",
+//
+//   // TODO: configure rskMainnet network settings
+//   // TODO: configure rskMainnet private key
+//   // TODO: configure rskTestnet network settings
+//   // TODO: configure rskTestnet private key
+//   
+// };
+	require("@nomiclabs/hardhat-ethers");
+	require('dotenv').config();
 
-  // TODO: configure rskMainnet network settings
-  // TODO: configure rskMainnet private key
-  // TODO: configure rskTestnet network settings
-  // TODO: configure rskTestnet private key
-  
-};
+<!-- Hardhat configuration -->
+  module.exports = {
+    solidity: "0.8.20",
+    networks: {
+      // rskMainnet: {
+      //   url: "https://public-node.rsk.co",
+      //   chainId: 30,
+      //   gasPrice: 60000000,
+      //   accounts: [process.env.ROOTSTOCK_MAINNET_PRIVATE_KEY]
+      // },
+      rskTestnet: {
+        url: "https://public-node.testnet.rsk.co",
+        chainId: 31,
+        gasPrice: 60000000,
+        accounts: [process.env.ROOTSTOCK_TESTNET_PRIVATE_KEY]
+      }
+    }
+  };
--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -1,4 +1,16 @@
+// TODO: Configure tests
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
-// TODO: Configure tests
+describe("MyToken", function () {
+it("Should deploy MyToken and assign the total supply to the owner", async function () {
+	const [owner] = await ethers.getSigners();
+
+	const MyToken = await ethers.getContractFactory("MyToken");
+	const myToken = await MyToken.deploy(1000);
+	await myToken.deployed();
+
+	expect((await myToken.totalSupply()).toString()).to.equal('1000');
+	expect((await myToken.balanceOf(owner.address)).toString()).to.equal('1000');
+		});
+});
